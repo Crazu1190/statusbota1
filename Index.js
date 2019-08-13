07c25149e1a3c018635541c19192255c2ee6ba3a
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-13 14:57:12 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('!pomoc', {type: 'WATCHING'});
+ 
 });
 
 client.login(process.env.TOKEN);
